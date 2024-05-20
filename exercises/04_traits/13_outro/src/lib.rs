a1cdb970953d0c3760378ac0dab4aeb6316de22b
Dave Cole <david.cole@data61.csiro.au>
2024-05-20 22:07:50 +0000

    Complete 04

--- exercises/04_traits/13_outro/src/lib.rs
+++ exercises/04_traits/13_outro/src/lib.rs
@@ -8,3 +8,62 @@
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
+
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct SaturatingU16 {
+    value: u16
+}
+
+impl From<u16> for SaturatingU16 {
+    fn from(value: u16) -> Self {
+        Self { value }
+    }
+}
+
+impl From<&u16> for SaturatingU16 {
+    fn from(value: &u16) -> Self {
+        (*value).into()
+    }
+}
+
+impl From<u8> for SaturatingU16 {
+    fn from(value: u8) -> Self {
+        Self { value: value.into() }
+    }
+}
+
+impl From<&u8> for SaturatingU16 {
+    fn from(value: &u8) -> Self {
+        (*value).into()
+    }
+}
+
+use core::ops::Add;
+
+impl Add for SaturatingU16 {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self {
+        Self {value: self.value.saturating_add(rhs.value) }
+    }
+}
+
+impl Add<u16> for SaturatingU16 {
+    type Output = Self;
+    fn add(self, rhs: u16) -> Self::Output {
+        self + Self::from(rhs)
+    }
+}
+
+impl Add<&u16> for SaturatingU16 {
+    type Output = Self;
+    fn add(self, rhs: &u16) -> Self::Output {
+        self + Self::from(rhs)
+    }
+}
+
+impl PartialEq<u16> for SaturatingU16 {
+    fn eq(&self, other: &u16) -> bool {
+        self.value == *other
+    }
+}
